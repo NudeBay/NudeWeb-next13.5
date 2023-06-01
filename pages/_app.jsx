6abfaacfe15ab0db558949a938026b01108487ef
Mikeal <mwiemszra@gmail.com>
2023-06-01 21:07:31 +0200

    ➕ Add ssr progressbar (swiper for future update)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import styles from '../assets/scss/global.scss';
+import NextNProgress from 'nextjs-progressbar';
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress color='#e5194d' startPosition={0.3} />
       <Navbar />
       <main className={`rightPanel`}>
         <Component {...pageProps} />
