a5a81d0b0304d48e9855e43f58e29dc29d94f5a0
Mikeal <mwiemszra@gmail.com>
2023-06-12 17:28:35 +0200

    login and register page

---
+++ pages/login.jsx
@@ -0,0 +1,7 @@
+import Sign from '../components/Sign';
+
+export default function Login() {
+    return (
+        <Sign type={'in'} />
+    );
+}
---
+++ pages/register.jsx
@@ -0,0 +1,7 @@
+import Sign from '../components/Sign';
+
+export default function Register() {
+    return (
+        <Sign type={'up'} />
+    );
+}
