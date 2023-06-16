f9869bdd7ad905498d82b40973cfed3378690bc9
Mikeal <mwiemszra@gmail.com>
2023-06-16 22:25:16 +0200

    🚧 Password forgot page

---
+++ pages/forgot.jsx
@@ -0,0 +1,9 @@
+import styles from '../assets/scss/Sign.module.scss';
+
+export default function Forgot() {
+    return (
+        <section className={styles.forgot}>
+            <h1>Function temporary unavailable</h1>
+        </section>
+    );
+}
