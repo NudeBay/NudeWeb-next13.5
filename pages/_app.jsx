80afd719a599119434e823216d48e9cd7479905b
Mikeal <mwiemszra@gmail.com>
2023-11-13 21:39:33 +0100

    ➕ Setup auth0

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,18 @@
 import styles from '../assets/scss/global.scss';
 import NextNProgress from 'nextjs-progressbar';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NextNProgress color='#e5194d' startPosition={0.3} />
-      <Navbar />
-      <main className={`rightPanel`}>
-        <Component {...pageProps} />
-      </main>
+      <UserProvider>
+        <NextNProgress color='#e5194d' startPosition={0.3} />
+        <Navbar />
+        <main className={`rightPanel`}>
+          <Component {...pageProps} />
+        </main>
+      </UserProvider>
     </>
   );
 }
